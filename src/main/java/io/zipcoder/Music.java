2ab8b001c80c144707c817ea8a6e8b2b974ee3e6
Zachary <zachary@Zipcoders-MacBook-Pro-2.local>
2023-03-30 17:08:33 -0400

    completed

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import java.util.Objects;
+
 public class Music {
 
     private String[] playList;
@@ -8,7 +10,47 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
+    public Integer selection(Integer startIndex, String selection) {
+        int buttonPressesUp = 0;
+        int buttonPressesDown = 0;
+        int k = startIndex;
+
+
+        for (int i = k; i < playList.length; i += 0) {
+            if (playList[i] != selection) {
+                i++;
+                buttonPressesDown += 1;
+            }else if (Objects.equals(playList[i], selection)) {
+                break;
+            }else if (i == playList.length - 1) {
+                i = 0;
+
+            }
+        }
+        for (int j = k; j < playList.length; j+=0){
+            if (j == 0) {
+                j = playList.length - 1;
+                buttonPressesUp += 1;
+                    if(playList[j] == selection){
+                        break;
+                    }
+            }
+            else if (playList[j] == selection) {
+                break;
+            } else if (playList[j] != selection ) {
+                    j--;
+                    buttonPressesUp += 1;
+            }
+        }
+
+        if (buttonPressesUp < buttonPressesDown){
+            return  buttonPressesUp;
+        } else if (buttonPressesDown <= buttonPressesUp) {
+            return buttonPressesDown;
+        }
         return null;
+        }
+
+
     }
-}
+
